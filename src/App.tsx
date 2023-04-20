7a5e502691e81e7a6e0e77d0110793c675ad7ddb
Kevin Qu <kevinqu88@gmail.com>
2023-04-19 23:15:02 -0400

    filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { Box, Stack } from '@mui/material';
+import { useState } from 'react';
+import AdvancedFilter, { FilterValue } from './Filter';
+
+export const PRODUCER_OPTIONS = ["Rick McCallum", "George Lucas", "Gary Kurtz", "Kevin Qu"];
+export const EP_OPTIONS = ["1", "2", "3", "4", "5", "6"];
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [filterValue, setFilterValue] = useState<FilterValue>({
+    crawlText: '',
+    titleText: '',
+    eps: {
+      [EP_OPTIONS[0]]: true,
+      [EP_OPTIONS[1]]: false,
+      [EP_OPTIONS[2]]: true,
+      [EP_OPTIONS[3]]: false,
+      [EP_OPTIONS[4]]: false,
+      [EP_OPTIONS[5]]: true,
+    },
+    producers: {
+      [PRODUCER_OPTIONS[0]]: true,
+      [PRODUCER_OPTIONS[1]]: false,
+      [PRODUCER_OPTIONS[2]]: true,
+      [PRODUCER_OPTIONS[3]]: false,
+    }
+  });
+
+  const handleFilterChange = (filters: any) => {
+    setFilterValue(filters);
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Stack direction="column" justifyContent="start" alignItems="start">
+      <Box>
+        Films
+      </Box>
+      <AdvancedFilter filters={ filterValue } onFilterChange={ handleFilterChange } />
+      <Box>
+        { JSON.stringify(filterValue) }
+      </Box>
+    </Stack>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Filter.tsx
@@ -0,0 +1,155 @@
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, FormLabel, IconButton, Input, InputAdornment, InputLabel, Stack } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import { EP_OPTIONS, PRODUCER_OPTIONS } from './App';
+import SendIcon from '@mui/icons-material/FilterList';
+import { useState } from 'react';
+
+
+export interface AdvancedFilterProps {
+  onFilterChange: (filterObj: FilterValue) => void;
+  filters: FilterValue;
+}
+
+export interface FilterValue {
+  producers: {
+    [key: string]: boolean
+  },
+  eps: {
+    [key: string]: boolean
+  },
+  crawlText: string,
+  titleText: string
+}
+
+function AdvancedFilter({ filters, onFilterChange }: AdvancedFilterProps) {
+  const [filterOpen, setFilterOpen] = useState<boolean>(true);
+  
+  const handleOpenAdvancedFilters = () => {
+    setFilterOpen((current) => {
+      return !current;
+    });
+  };
+
+  const handleFilterChange = (filterKey: string) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    let val = {
+      ...filters
+    };
+
+    if (filterKey === 'crawlText' || filterKey === 'titleText') {
+      val = {
+        ...filters,
+        [filterKey]: event.target.value
+      };
+    } else {
+      val = {
+        ...filters,
+        [filterKey]: {
+          ...filters[filterKey as keyof FilterValue] as {[key: string]: boolean},
+          [event.target.name]: event.target.checked
+        }
+      };
+    }
+    onFilterChange(val);
+  };
+
+  const clearFilterForText = (filterKey: string) => (e: any) => {
+  };
+
+  return (
+    <Stack direction="column" justifyContent="center" alignItems="center" >
+
+      <Stack direction="row" justifyContent="start" alignItems="center" width="100%" spacing={ 4 }>
+        <FormControl variant="standard">
+          <InputLabel>Title</InputLabel>
+          <Input
+            type='text'
+            value={ filters.titleText }
+            onChange={ handleFilterChange('titleText') }
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={ clearFilterForText('titleText') }
+                >
+                  <CloseIcon />
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Button variant="contained" endIcon={ <SendIcon /> } onClick={ handleOpenAdvancedFilters }>
+          Open Advanced Filter
+        </Button>
+      </Stack>
+      
+      {
+        filterOpen && (
+          <Box width="100%" border="1px solid #ccc" mt={ 3 }>
+            <Stack direction="row" justifyContent="start" alignItems="start" width="100%" p={ 3 }>
+              <FormControl sx={ { m: 3 } } component="fieldset" variant="standard">
+                <FormLabel component="legend" 
+                  sx={ {display: 'flex', justifyContent:"start", alignItems:"start", width:'100%'} } >Producers</FormLabel>
+                <FormGroup>
+                  {
+                    PRODUCER_OPTIONS.map((producerOption) => {
+                      return (
+                        <FormControlLabel key={ producerOption }
+                          control={
+                            <Checkbox checked={ filters.producers[producerOption] } onChange={ handleFilterChange('producers') } name={ producerOption } />
+                          }
+                          label={ producerOption }
+                        />
+                      );
+                    })
+                  }
+                </FormGroup>
+              </FormControl>
+
+              <FormControl sx={ { m: 3 } } component="fieldset" variant="standard">
+                <FormLabel component="legend" 
+                  sx={ {display: 'flex', justifyContent:"start", alignItems:"start", width:'100%'} } >Ep.</FormLabel>
+                <FormGroup>
+                  {
+                    EP_OPTIONS.map((epOption) => {
+                      return (
+                        <FormControlLabel key={ epOption }
+                          control={
+                            <Checkbox checked={ filters.eps[epOption] } onChange={ handleFilterChange('eps') } name={ epOption } />
+                          }
+                          label={ epOption }
+                        />
+                      );
+                    })
+                  }
+                </FormGroup>
+              </FormControl>
+
+              <FormControl variant="standard">
+                <InputLabel>Crawl Text</InputLabel>
+                <Input
+                  type='text'
+                  value={ filters.crawlText }
+                  onChange={ handleFilterChange('crawlText') }
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={ clearFilterForText('crawlText') }
+                      >
+                        <CloseIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                />
+              </FormControl>
+
+            </Stack>
+          </Box>
+        )
+      }
+      
+
+    </Stack>
+    
+  );
+}
+
+export default AdvancedFilter;
